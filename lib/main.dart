05f665870c6dd8a08137e303281cc8c34b81003c
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-10-28 20:14:04 +0700

    update nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
-import '/home/view/home_page.dart'; 
+import 'package:budi_rahayu_care_app/home/view/home_page.dart';
+import 'package:budi_rahayu_care_app/about/view/tentang_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(), 
+      title: 'Budi Rahayu Care',
+      initialRoute: '/', 
+      routes: {
+        '/': (context) => const HomePage(),
+        '/about': (context) => const TentangPage(),
+      },
     );
   }
 }
