591002f9a445dfbdc6f4cf0934ea1d8f5f0e2fdb
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-11-02 20:58:27 +0700

    Cleaned up Firebase config to Android & Web only, Stop tracking firebase_options.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 import 'package:budi_rahayu_care_app/home/view/home_page.dart';
 import 'package:budi_rahayu_care_app/contact/view/kontak_kami_page.dart';
@@ -14,6 +15,7 @@ import 'package:budi_rahayu_care_app/admin/documentation/admin_edit_docs_page.da
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
