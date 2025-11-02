2a9b34dc0ef6524fe060e5023a57f4376ee8ac5e
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-11-02 16:26:59 +0700

    Create admin_edit_docs_page.dart, add Firebase initialization and FlutterFire config support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:budi_rahayu_care_app/home/view/home_page.dart';
 import 'package:budi_rahayu_care_app/contact/view/kontak_kami_page.dart';
 import 'package:budi_rahayu_care_app/about/view/tentang_page.dart';
@@ -8,9 +10,13 @@ import 'package:budi_rahayu_care_app/admin/admin_login_page.dart';
 import 'package:budi_rahayu_care_app/admin/dashboard/admin_dashboard_page.dart';
 import 'package:budi_rahayu_care_app/admin/children/view/admin_children_page.dart';
 import 'package:budi_rahayu_care_app/admin/news/admin_news_page.dart'; 
-import 'package:budi_rahayu_care_app/admin/documentation/admin_docs_page.dart';
+import 'package:budi_rahayu_care_app/admin/documentation/admin_edit_docs_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
